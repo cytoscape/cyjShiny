34624b1ba237d15a858ba81a3c74de26a3ae043f
oshah97 <oshah97@outlook.com>
2018-08-03 11:13:56 -0700

    working styling; loaded from within .js

--- inst/browserCode/src/cyjShiny.js
+++ inst/browserCode/src/cyjShiny.js
@@ -31,8 +31,8 @@ HTMLWidgets.widget({
 		var data = JSON.parse(x.message)
 		console.log(data);
 		console.log(typeof data)
-		console.log(length(data.elements.nodes))
-		console.log(length(data.elements.edges))
+		console.log(data.elements.nodes)
+		console.log(data.elements.edges)
 		
 		var cyDiv = el;
 		//htmlElement = el;
@@ -41,7 +41,46 @@ HTMLWidgets.widget({
 		    container: cyDiv,
 		    elements: data.elements,
 		    layout: {name: 'random'},
-		    		    
+		    style: [{selector: "node", css: {
+			"shape": "ellipse",
+			"text-valign":"center",
+			"text-halign":"center",
+			"content": "data(label)",
+			"border-width": "3px",
+			"background-color": "#FFFFFF",
+			"border-color":"black",
+			"width": "80px",
+			"height": "80px",
+			"font-size":"24px"}},
+		    
+		    
+		    {selector:"node:selected", css: {
+			"text-valign":"center",
+			"text-halign":"center",
+			"border-color": "black",
+			"content": "data(id)",
+			"border-width": "3px",
+			"overlay-opacity": 0.2,
+			"overlay-color": "gray"
+		    }},
+		    
+		    
+		    {selector:"edge", css: {
+			"line-color": "rgb(50,50,50",
+			'target-arrow-color': 'rgb(50,50,50)',
+			'target-arrow-shape': 'triangle',
+			"width": "1px",
+			'curve-style': 'bezier',
+			'haystack-radius': 0.5
+		    }},
+
+
+		    {"selector": "edge:selected", css: {
+			"overlay-opacity": 0.2,
+			"overlay-color": "gray",
+			"width": "2px",
+		    }}],
+		    
 		    ready: function(){
 			$("#cyjShiny").height(0.8 * window.innerHeight);
 			var cyj = this;
