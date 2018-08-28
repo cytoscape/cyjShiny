887c512b69f102da845f69488f68c2220663a621
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-08-28 12:25:24 -0700

    passes CHECK clean

--- R/cyjShiny.R
+++ R/cyjShiny.R
@@ -1,84 +1,86 @@
-#----------------------------------------------------------------------------------------------------
-#' Constructor for the cyjShiny widget
+#' @importFrom jsonlite toJSON fromJSON
+#' @importFrom htmlwidgets createWidget shinyWidgetOutput shinyRenderWidget
+#' @import shiny
 #'
-#' @param graph an R graphNEL instance (igraph support coming soon)
-#' @param width integer  initial width of the widget
-#' @param height integer initial height of the widget
-#' @param elementId string the DOM id into which the widget is rendered, default NULL is best
+#' @title cyjShiny
+#------------------------------------------------------------------------------------------------------------------------
+#' cyjShiny
 #'
-#' @return a reference to an htmlwidget
-#'
-#' @export
-#'
-#' @examples
-#'   in ui:
-#'     mainPanel(cyjShinyOutput('cyjShiny'), width=10)
-#'
-#'   in server:
-#'      ouptu$cyjShiny <- renderCyjShiny(cyjShiny(graph))
-#'
+#' @description
+#' This widget wraps cytoscape.js, a full-featured Javsscript network library for visualization and analysis.
 #'
 #' @aliases cyjShiny
 #' @rdname cyjShiny
-#----------------------------------------------------------------------------------------------------
+#'
+#' @param graph an R graphNEL instance (igraph support coming soon).
+#' @param width integer  initial width of the widget.
+#' @param height integer initial height of the widget.
+#' @param elementId string the DOM id into which the widget is rendered, default NULL is best.
+#'
+#' @return a reference to an htmlwidget.
+#'
+#'
+#' @examples
+#' \dontrun{
+#'   output$cyjShiny <- renderCyjShiny(cyjShiny(graph))
+#' }
+#'
+#' @export
+
+
 cyjShiny <- function(graph, width = NULL, height = NULL, elementId = NULL)
 {
-    printf("--- ~/github/cyjShiny/R/cyjShiny ctor");
-    x <- list(
-        graph = graph
+    x <- list(graph = graph)
+
+    htmlwidgets::createWidget(
+       name = 'cyjShiny',
+       x,
+       width = width,
+       height = height,
+                     package = 'cyjShiny',
+       elementId = elementId
     )
 
-    # create widget
-    htmlwidgets::createWidget(
-                     name = 'cyjShiny',
-                     x,
-                     width = width,
-                     height = height,
-                     package = 'cyjShiny',
-                     elementId = elementId
-                 )
-
 } # cyjShiny constructor
-#----------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------
 #' Standard shiny ui rendering construct
 #'
-#' @param outputID the name of the DOM element to create
-#' @param width integer  optional initial width of the widget
-#' @param height integer optional initial height of the widget
+#' @param outputId the name of the DOM element to create.
+#' @param width integer  optional initial width of the widget.
+#' @param height integer optional initial height of the widget.
 #'
 #' @return a reference to an htmlwidget
 #'
 #' @export
 #'
 #' @examples
-#'   in ui:
-#'     mainPanel(cyjShinyOutput('cyjShiny'), width=10)
-#'
-#'   in server:
-#'      ouptu$cyjShiny <- renderCyjShiny(cyjShiny(graph))
-#'
+#' \dontrun{
+#'   mainPanel(cyjShinyOutput('cyjShiny'), width=10)
+#' }
 #'
 #' @aliases cyjShinyOutput
 #' @rdname cyjShinyOutput
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 cyjShinyOutput <- function(outputId, width = '100%', height = '400px')
 {
-  htmlwidgets::shinyWidgetOutput(outputId, 'cyjShiny', width, height, package = 'cyjShiny')
+    htmlwidgets::shinyWidgetOutput(outputId, 'cyjShiny', width, height, package = 'cyjShiny')
 }
-#----------------------------------------------------------------------------------------------------
-#' More shiny plumbing - now sure exactly what this does
+#------------------------------------------------------------------------------------------------------------------------
+#' More shiny plumbing -  a cyjShiny wrapper for htmlwidget standard rendering operation
 #'
-#' @param expr not sure...
-#' @param env environment not sure ...
-#' @param quoteed logical not sure ...
+#' @param expr an expression that generates an HTML widget.
+#' @param env environment in which to evaluate expr.
+#' @param quoted logical specifies whether expr is quoted ("useuful if you want to save an expression in a variable").
 #'
 #' @return not sure
 #'
-#' @export
-#'
 #' @aliases renderCyjShiny
 #' @rdname renderCyjShiny
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 renderCyjShiny <- function(expr, env = parent.frame(), quoted = FALSE)
 {
    if (!quoted){
@@ -88,27 +90,30 @@ renderCyjShiny <- function(expr, env = parent.frame(), quoted = FALSE)
   htmlwidgets::shinyRenderWidget(expr, cyjShinyOutput, env, quoted = TRUE)
 
 }
-#----------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------
 #' load a standard cytoscape.js style file
 #'
-#' @param filename character string, either relative or absolute path
+#' @param filename character string, either relative or absolute path.
 #'
 #' @return nothing
 #'
 #' @export
 #'
 #' @examples
-#'
-#' loadStyleFile(system.file(package="cyjShiny", "extdata", "yeastGalactoseStyle.js"))
+#' \dontrun{
+#'   loadStyleFile(system.file(package="cyjShiny", "extdata", "yeastGalactoseStyle.js"))
+#' }
 #'
 #' @aliases loadStyleFile
 #' @rdname loadStyleFile
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 loadStyleFile <- function(filename)
 {
    if(!file.exists(filename)){
-      printf("cannot read style file: %s", filename)
-      return;
+      warning(sprintf("cannot read style file: %s", filename))
+      return();
       }
 
    jsonText <- toJSON(fromJSON(filename))
@@ -119,22 +124,23 @@ loadStyleFile <- function(filename)
 
 } # loadStyleFile
 #------------------------------------------------------------------------------------------------------------------------
-#' Set zoom and center of the graph display so that graph fills the display
+#' Set zoom and center of the graph display so that graph fills the display.
 #'
-#' @param session a Shiny server session object
-#' @param padding integer, default 50 pixels
-#'
-#' @export
+#' @param session a Shiny server session object.
+#' @param padding integer, default 50 pixels.
 #'
 #' @examples
-#'
+#' \dontrun{
 #'   fit(session, 100)
+#'}
 #'
 #' @aliases fit
 #' @rdname fit
 #'
 #' @seealso\code{\link{fitSelected}}
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 fit <- function(session, padding=50)
 {
    session$sendCustomMessage("fit", list(padding=padding))
@@ -143,45 +149,47 @@ fit <- function(session, padding=50)
 #------------------------------------------------------------------------------------------------------------------------
 #' Set zoom and center of the graph display so that the currently selected nodes fill the display
 #'
-#' @param session  a Shiny server session object
-#' @param padding integer, default 50 pixels
-#'
-#' @export
+#' @param session  a Shiny server session object.
+#' @param padding integer, default 50 pixels.
 #'
 #' @examples
-#'
+#' \dontrun{
 #'   fitSelected(session, 100)
+#' }
 #'
 #' @aliases fitSelected
 #' @rdname fitSelected
 #'
 #' @seealso\code{\link{fit}}
 #'
-#----------------------------------------------------------------------------------------------------
+#' @export
+
 fitSelected <- function(session, padding=50)
 {
    session$sendCustomMessage("fitSelected", list(padding=padding))
 
 } # fitSelected
 #------------------------------------------------------------------------------------------------------------------------
-#' Assign the supplied node attribute values to the graph structure contained in the browser
+#' Assign the supplied node attribute values to the graph structure contained in the browser.
 #'
-#' @param session a Shiny Server session object
-#' @param attributeName character string, the attribute to update
-#' @param nodes a character vector the names of the nodes whose attributes are updated
-#' @param values a character, logical or numeric vector, the new values
-#'
-#' @export
+#' @param session a Shiny Server session object.
+#' @param attributeName character string, the attribute to update.
+#' @param nodes a character vector the names of the nodes whose attributes are updated.
+#' @param values a character, logical or numeric vector, the new values.
 #'
 #' @examples
+#' \dontrun{
 #'   setNodeAttributes(session,
 #'                     attributeName=attribute,
 #'                     nodes=yeastGalactodeNodeIDs,
 #'                     values=expression.vector)
+#' }
 #'
 #' @aliases setNodeAttributes
 #' @rdname setNodeAttributes
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 setNodeAttributes <- function(session, attributeName, nodes, values)
 {
    session$sendCustomMessage(type="setNodeAttributes",
@@ -192,17 +200,18 @@ setNodeAttributes <- function(session, attributeName, nodes, values)
 #------------------------------------------------------------------------------------------------------------------------
 #' Layout the current graph using the specified strategy.
 #'
-#' @param session a Shiny Server session object
-#' @param layoutStrategy character string, one of cola, cose, circle, concentric, grid, breadthfirst, random, dagre, cose-bilkent
-#'
-#' @export
+#' @param session a Shiny Server session object.
+#' @param strategy a character string, one of cola, cose, circle, concentric, grid, breadthfirst, random, dagre, cose-bilkent.
 #'
 #' @examples
+#' \dontrun{
 #'   doLayout(session, "cola")
-#'
+#' }
 #' @aliases doLayout
 #' @rdname doLayout
-#----------------------------------------------------------------------------------------------------
+#'
+#' @export
+
 doLayout <- function(session, strategy)
 {
    stopifnot(strategy %in% c("cola", "cose", "circle", "concentric", "grid", "breadthfirst", "random",
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all:  docs install
+all:  npm docs install build check
 
 docs:
 	R -e "devtools::document()"
@@ -9,13 +9,13 @@ npm:
 	(cd inst/browserCode; make)
 
 build:
-	(cd ..; R CMD build --no-build-vignettes RCyjs)
+	(cd ..; R CMD build --no-build-vignettes cyjShiny)
 
 install:
 	(cd ..; R CMD INSTALL cyjShiny)
 
 check:
-	(cd ..; R CMD check `ls -t RCyjs_* | head -1`)
+	(cd ..; R CMD check `ls -t cyjShiny_* | head -1`)
 
 biocCheck:
-	(cd ..; R CMD BiocCheck `ls -t RCyjs_* | head -1`)
+	(cd ..; R CMD BiocCheck `ls -t cyjShiny_* | head -1`)
