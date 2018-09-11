72336f1daf117a579411ab71c5911ec7f8cfcfd9
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-09-11 10:58:07 -0700

    graphToJSON moved from tinyApp.R to package proper

--- R/cyjShiny.R
+++ R/cyjShiny.R
@@ -241,3 +241,93 @@ doLayout <- function(session, strategy)
 
 } # doLayout
 #------------------------------------------------------------------------------------------------------------------------
+#' Convert R graphNEL object to cytoscape.js JSON.
+#'
+#' @param g  a graphNEL
+#'
+#' @examples
+#' \dontrun{
+#'   g.json <- graphToJSON(graphNEL())
+#' }
+#'
+#' @aliases graphToJSON
+#' @rdname graphToJSON
+#'
+#' @export
+
+graphToJSON <- function(g) #Copied from RCyjs/R/utils.R
+{
+   if(length(nodes(g)) == 0)
+      return ("{}")
+
+       # allocate more character vectors that we could ever need; unused are deleted at conclusion
+
+    vector.count <- 10 * (length(edgeNames(g)) + length (nodes(g)))
+    vec <- vector(mode="character", length=vector.count)
+    i <- 1;
+
+    vec[i] <- '{"elements": {"nodes": ['; i <- i + 1;
+    nodes <- nodes(g)
+    edgeNames <- edgeNames(g)
+    edges <- strsplit(edgeNames, "~")  # a list of pairs
+    edgeNames <- sub("~", "->", edgeNames)
+    names(edges) <- edgeNames
+
+    noa.names <- names(graph::nodeDataDefaults(g))
+    eda.names <- names(graph::edgeDataDefaults(g))
+    nodeCount <- length(nodes)
+    edgeCount <- length(edgeNames)
+
+    for(n in 1:nodeCount){
+       node <- nodes[n]
+       vec[i] <- '{"data": '; i <- i + 1
+       nodeList <- list(id = node)
+       this.nodes.data <- graph::nodeData(g, node)[[1]]
+       if(length(this.nodes.data) > 0)
+          nodeList <- c(nodeList, this.nodes.data)
+       nodeList.json <- toJSON(nodeList, auto_unbox=TRUE)
+       vec[i] <- nodeList.json; i <- i + 1
+       if(all(c("xPos", "yPos") %in% names(graph::nodeDataDefaults(g)))){
+          position.markup <- sprintf(', "position": {"x": %f, "y": %f}',
+                                     graph::nodeData(g, node, "xPos")[[1]],
+                                     graph::nodeData(g, node, "yPos")[[1]])
+          vec[i] <- position.markup
+          i <- i + 1
+          }
+        if(n != nodeCount){
+           vec [i] <- "},"; i <- i + 1 # sprintf("%s},", x)  # another node coming, add a comma
+           }
+       } # for n
+
+    vec [i] <- "}]"; i <- i + 1  # close off the last node, the node array ], the nodes element }
+
+    if(edgeCount > 0){
+       vec[i] <- ', "edges": [' ; i <- i + 1
+       for(e in seq_len(edgeCount)) {
+          vec[i] <- '{"data": '; i <- i + 1
+          edgeName <- edgeNames[e]
+          edge <- edges[[e]]
+          sourceNode <- edge[[1]]
+          targetNode <- edge[[2]]
+          edgeList <- list(id=edgeName, source=sourceNode, target=targetNode)
+          this.edges.data <- edgeData(g, sourceNode, targetNode)[[1]]
+          if(length(this.edges.data) > 0)
+             edgeList <- c(edgeList, this.edges.data)
+          edgeList.json <- toJSON(edgeList, auto_unbox=TRUE)
+          vec[i] <- edgeList.json; i <- i + 1
+          if(e != edgeCount){          # add a comma, ready for the next edge element
+             vec [i] <- '},'; i <- i + 1
+             }
+          } # for e
+      vec [i] <- "}]"; i <- i + 1
+      } # if edgeCount > 0
+
+   vec [i] <- "}"  # close the edges object
+   i <- i + 1;
+   vec [i] <- "}"  # close the elements object
+   vec.trimmed <- vec [which(vec != "")]
+   #printf("%d strings used in constructing json", length(vec.trimmed))
+   paste0(vec.trimmed, collapse=" ")
+
+} # graphToJSON
+#----------------------------------------------------------------------------------------------------------
