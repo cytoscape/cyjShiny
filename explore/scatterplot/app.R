e9ab30c09bed58e8e43c8fc3737c6bb0ef9a67de
Dharna5599 <dharnachandrakar@gmail.com>
2021-06-14 19:11:08 +0530

    Update app.R

--- explore/scatterplot/app.R
+++ explore/scatterplot/app.R
@@ -1,55 +1,26 @@
+
 library(shiny)
+set.seed(17)
 
-ui <- shinyUI(fluidPage(
-  br(),
-  actionButton("numb", "generate a random plot graph"),
-  br(),
-  br(),
-  verbatimTextOutput("text"),
-  plotOutput("plot"),
-  plotOutput("plot2")
-))
+ui <- fluidPage(
+    titlePanel("Scatterplot Demo"),
+        actionButton("newPlotButton", "New Plot"),
+        plotOutput(outputId = "scatterplot")
+    )
 
-server <- shinyServer(function(input, output) {
-  
-  model <- eventReactive(input$numb, {
-    # draw a random number and print it
-    random <- sample(1:100, 1)
-    print(paste0("The number is: ", random))
+
+server <- function(input, output) {
+
+    output$scatterplot <- renderPlot({
+      input$newPlotButton   # just this mention of #newPlotButton triggers this block of code
+      colors <- c("red", "blue", "green", "orange", "brown", "black")
+      point.color <- sample(colors, 1)
+      xPoints <- sample(1:100, replace=TRUE)
+      yPoints <- sample(1:100,  replace=TRUE)
+      plot(xPoints, yPoints, col=point.color)
+      })
     
-    # create a plot 
-    set.seed(17)
-    Plot <- plot(sample(1:10,1:10))
     
-    # create a second plot
-    set.seed(17)
-    Plot2 <- plot(sample(1:100,5), sample(1:100,5))
-    
-    # return all object as a list
-    list(random = random, Plot = Plot, Plot2=Plot2)
-  })
-  
-  output$text <- renderText({
-    # print the random number after accessing "model" with brackets.
-    # It doesn't re-run the function.
-    youget <- paste0("After using model()$random you get: ", model()$random,
-                     ". Compare it")
-    print(youget)
-    youget
-  })
-  
-  output$plot <- renderPlot({
-    # render saved plot
-    set.seed(17)
-    Plot2 <- plot(sample(1:100,5), sample(1:100,5))
-  })
-  
-  output$plot2 <-renderPlot({
-    set.seed(17)
-    Plot <- plot(sample(1:10,1:10))
-  })
+}
 
-})
-
-
-shinyApp(ui = ui, server = server)
+app <- shinyApp(ui = ui, server = server)
