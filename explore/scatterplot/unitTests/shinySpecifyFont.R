88a1d7ffb4f0d215f2f2a965ad331c52d071333f
Dharna5599 <dharnachandrakar@gmail.com>
2021-06-24 01:06:44 +0530

    Specify shiny font

--- explore/scatterplot/unitTests/shinySpecifyFont.R
+++ explore/scatterplot/unitTests/shinySpecifyFont.R
@@ -1,1 +1,34 @@
 # shinySpecifyFont.R
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    tags$head(
+        # Note the wrapping of the string in HTML()
+        tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+      body {
+        background-color: white;
+        color: black;
+      }
+      h2 {
+        font-family: 'Yusei Magic', sans-serif;
+      }
+      .shiny-input-container {
+        color: #474747;
+      }"))
+    ),
+
+    # Application title
+    titlePanel("Shiny specify font")
+)
+
+# Define server logic 
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
