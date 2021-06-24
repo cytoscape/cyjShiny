e6b56531e268888cfcebb534b7e740264317fca7
Dharna5599 <dharnachandrakar@gmail.com>
2021-06-24 19:49:36 +0530

    Added font style and re-recorded test
    Added fixed shiny font style in dharnashinyTestDemonstration/app.R and re-recorded the test.

--- explore/scatterplot/unitTests/dharnashinyTestDemonstration/app.R
+++ explore/scatterplot/unitTests/dharnashinyTestDemonstration/app.R
@@ -1,6 +1,21 @@
 library(shiny)
 
 ui <- fluidPage(
+        tags$head(
+        # Note the wrapping of the string in HTML()
+        tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+      body {
+        background-color: white;
+        color: black;
+      }
+      h2 {
+        font-family: 'Yusei Magic', sans-serif;
+      }
+      .shiny-input-container {
+        color: #474747;
+      }"))
+    ),
     titlePanel("Scatterplot Demo"),
        actionButton("newPlotButton", "New Plot"),
        plotOutput(outputId = "scatterplot")
