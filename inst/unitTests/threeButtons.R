16e6893165033c0da946d2cb164af36d6fe71bb7
Dharna5599 <dharnachandrakar@gmail.com>
2021-07-19 23:18:09 +0530

    threeButtons

--- inst/unitTests/threeButtons.R
+++ inst/unitTests/threeButtons.R
@@ -1,73 +1,58 @@
 library(shiny)
 library(R6)
 
-#------------------------------------------------------------------------
-MyModule <- R6Class(
+MyModule = R6Class("MyModule",
+    #--------------------------------------------------------------------------------
+    public = list(
+      initialize = function(){
+      message(sprintf("initializing demo"))
+      },
+                          
+      #------------------------------------------------------------
+      ui = function(){
+        fluidPage(
+          titlePanel(title="three button test"),
+          br(),                                                       # empty line
+          actionButton(inputId = "ClickonMe", label = "Make Text"),   # button 1
+          actionButton(inputId = "ClickonMe2", label = "Print Text"), # button 2
+          actionButton(inputId = "ClickonMe3", label = "Clear Text"),
+          mainPanel(verbatimTextOutput("Responsetext"))
+           # sidebarLayout
+          )}, # ui
+                          
+      #------------------------------------------------------------
+      server = function(input, output, session){
+        values <- reactiveValues()
+        values$name <- NULL
+                            
+        observeEvent(input$ClickonMe, ignoreInit=TRUE, {
+          values$name <- T
+        })
 
-  #-------------------------------------------------------------
-  public = list(
-    initialize = function(id = shiny:::createUniqueId()){
-      private$id <- id
-    },
-
-    bind = function(){
-      callModule(private$moduleserver, private$id)
-    },
-  #--------------------------------------------------------------------------
-    ui = function(ns = NS(NULL)){
-      ns <- NS(ns(private$id))
-      fluidPage(
-        h3("Hello there"),                                          # First text on the window
-        br(),                                                       # empty line
-        actionButton(inputId = ns("ClickonMe"), label = "Make Text"),   # button 1
-        actionButton(inputId = ns("ClickonMe2"), label = "Print Text"), # button 2
-        actionButton(inputId = ns("ClickonMe3"), label = "Clear Text"),
-        mainPanel(verbatimTextOutput(ns("Responsetext")))
-      )
-    }
-  ),
-#----------------------------------------------------------------------------------
-  private = list(
-    id = NULL,
-    moduleserver = function(input, output, session){
-      ns <- session$ns
-      values <- reactiveValues()
-      values$name <- NULL
-  #------------------------------------------------------------------------------------    
-      observeEvent(input$ClickonMe,{
-        values$name <- T 
-      })
-  #-------------------------------------------------------------------------------------    
-      observeEvent(input$ClickonMe3,{
-        if (values$name){ 
-          values$name <- F
+        observeEvent(input$ClickonMe3,{
+          if (values$name){ 
+            values$name <- F
           }
-      })
-  #-------------------------------------------------------------------------------------    
-      observeEvent(input$ClickonMe2,{
-        if (values$name){
-          print(values$name)
-        }
-      }) 
-  #-------------------------------------------------------------------------------------    
-      output$Responsetext <- renderPrint({
-        req(values$name)
-        if(!values$name){
-          removeUI(
+        })
+        observeEvent(input$ClickonMe2,{
+          if (values$name){
+            print(values$name)
+          }
+        }) 
+                            
+        output$Responsetext <- renderPrint({
+          req(values$name)
+          if(!values$name){
+            removeUI(
             selector = "div:has(> #Responsetext)"
-          )
-        }
-        as.character(values$name)})
-    }
-  )
-)
-#------------------------------------------------------------------------------------------
-
-myObj <- MyModule$new()
-
-runApp(
-shinyApp(
-  myObj$ui(),
-  function(input, output, session){ myObj$bind() }
-),
-port=9999, launch.browser=TRUE)
+            )
+          }
+          as.character(values$name)})
+                            
+      } # server
+                          
+    ) # public
+) # class
+#--------------------------------------------------------------------------------
+x <- MyModule$new()
+runApp(shinyApp(x$ui, x$server), port=9999, launch.browser=TRUE)
