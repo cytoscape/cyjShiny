a3f849b4224e0017c97e12da967e0c797a58bb11
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-08-23 08:33:09 -0700

    removed code used to assess (and subsequently abandon) shinytest

--- explore/scatterplot/unitTests/shinyTestDemonstration/tests/shinytest/test01.R
+++
@@ -1,8 +0,0 @@
-app <- ShinyDriver$new("../../")
-app$snapshotInit("test01")
-
-app$snapshot()
-app$setInputs(newPlotButton = "click")
-app$snapshot()
-app$setInputs(newPlotButton = "click")
-app$snapshot()
