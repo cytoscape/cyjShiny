c4376510e9dafc06434a668697c43a4953422240
cannin <cannin@gmail.com>
2023-03-22 16:52:18 -0400

    Updated vignette with examples

---
+++ inst/demos/rmarkdownDemo/preset_style.js
@@ -0,0 +1,12 @@
+[
+  {"selector":"node", "css": {
+    "border-width": "2px",
+    "border-color": "magenta",
+    "width": "48px",
+    "height": "48px",
+    "background-color": "magenta"
+  }},
+  {"selector": "edge[interaction='interacts with']", "css": {
+    "line-color": "magenta"
+  }}
+]
